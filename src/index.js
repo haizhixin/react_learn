712a1a692026ec426bd761515758e29253476e1d
zhangyaqiang <1126917181@qq.com>
2019-12-11 19:20:17 +0800

    照片切换

--- src/index.js
+++ src/index.js
@@ -1,12 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import './index.css'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import src1 from './assest/1.jpg';
+import src2 from './assest/2.jpg';
+import src3 from './assest/3.jpg';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+let srcs = [src1, src2, src3];
+let container = document.getElementById("root");
+let index = 0;
+
+
+function render() {
+    ReactDOM.render(<img src={srcs[index]} alt="" />, container);
+}
+let timer;
+function start() {
+    stop()
+    timer = setInterval(() => {
+
+        index++;
+        index = (index % 3)
+        render()
+    }, 1000)
+}
+
+function stop() {
+    clearInterval(timer)
+}
+//鼠标进入图片切换
+container.onmouseenter = () => {
+    start()
+}
+// 鼠标移出图片停止切换
+container.onmouseleave = () => {
+    stop()
+}
+
+
+
+
+
